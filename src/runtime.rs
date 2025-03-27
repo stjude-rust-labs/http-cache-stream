39a7948ed06a50ae18efbdae6bc780d04f44d5a1
sk <84185378+sauhardh@users.noreply.github.com>
2025-03-27 20:39:53 +0545

    fix: replace depricated async-std with smol (#9)
    * fix: replace depricated async-std with smol
    
    * nit: added whitespace.And fix(CI): update to github workflow replacing new feature flag
    
    * fix: reupdating visibility of runtime module

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,10 +1,10 @@
 //! Implementation of the async runtime integration.
 
-#[cfg(not(any(feature = "tokio", feature = "async-std")))]
-compile_error!("either feature `tokio` or `async-std` must be enabled");
+#[cfg(not(any(feature = "tokio", feature = "smol")))]
+compile_error!("either feature `tokio` or `smol` must be enabled");
 
-#[cfg(all(feature = "tokio", feature = "async-std"))]
-compile_error!("features `tokio` and `async-std` are mutually exclusive");
+#[cfg(all(feature = "tokio", feature = "smol"))]
+compile_error!("features `tokio` and `smol` are mutually exclusive");
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "tokio")] {
@@ -15,9 +15,9 @@ cfg_if::cfg_if! {
         pub fn unwrap_task_output<T>(result: Result<T, tokio::task::JoinError>) -> Option<T> {
             result.ok()
         }
-    } else if #[cfg(feature = "async-std")] {
-        pub use async_std::fs::File;
-        pub use async_std::task::spawn_blocking;
+    } else if #[cfg(feature = "smol")] {
+        pub use smol::fs::File;
+        pub use smol::unblock as spawn_blocking;
 
         /// Helper for difference in `spawn_blocking` signatures.
         pub fn unwrap_task_output<T>(result: T) -> Option<T> {
